deaf5648efc1457c40339a15d0ca334a6b16c08d
Niki Gawlik <gamedevniki@gmail.com>
2023-05-26 16:32:05 +0200

    Clean up vite.config.js a little bit

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,11 @@
 import { svelte } from '@sveltejs/vite-plugin-svelte'
-import { defineConfig } from 'vite'
-import { viteSingleFile } from 'vite-plugin-singlefile'
-import { resolve } from 'path'
-import mkcert from 'vite-plugin-mkcert'
 import { readFileSync } from 'fs'
-
-
+import { defineConfig } from 'vite'
+import mkcert from 'vite-plugin-mkcert'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // plugins: [svelte(), viteSingleFile({ useRecommendedBuildConfig: false })],
-  // plugins: [svelte()],
   plugins: [mkcert(), svelte(), viteSingleFile()],
   base: "",
   server: {
