fafb1d76dff2655cf71c3791d0aa6d523a059ae8
Niki Gawlik <gamedevniki@gmail.com>
2023-05-26 16:45:16 +0200

    Simplify vite config again, no experiments :p

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
 import { svelte } from '@sveltejs/vite-plugin-svelte'
-import { readFileSync } from 'fs'
 import { defineConfig } from 'vite'
 import mkcert from 'vite-plugin-mkcert'
 import { viteSingleFile } from 'vite-plugin-singlefile'
@@ -12,18 +11,6 @@ export default defineConfig({
     https: true,
   },
   build: {
-    rollupOptions: {
-      output: {
-        dir: getOutputDir(),
-      },
-    },
     minify: false
   }
 })
-
-function getOutputDir() {
-  const packageJson = JSON.parse(readFileSync('./package.json'));
-  const version = packageJson.version;
-
-  return `dist/${version}`;
-}
